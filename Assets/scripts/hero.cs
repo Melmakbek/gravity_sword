f355323d335345d1a5d7a3e39c1b87a998ac0240
Melmakbek <124694361+Melmakbek@users.noreply.github.com>
2024-07-21 14:11:01 +0300

    first steps

---
+++ Assets/scripts/hero.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int lives = 5;
+    [SerializeField] private float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if(Input.GetButton("Horizontal"))
+            Run();
+        if(Input.GetButtonDown("Jump"))
+            Jump();
+    }
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+}

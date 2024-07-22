b3c748e891e74a200128a70982cb051a91d5ae26
Melmakbek <124694361+Melmakbek@users.noreply.github.com>
2024-07-22 18:26:48 +0300

    tilrmap and jumpsbuffer not working

--- Assets/scripts/hero.cs
+++ Assets/scripts/hero.cs
@@ -8,19 +8,30 @@ public class hero : MonoBehaviour
     [SerializeField] private float speed = 3f;
     [SerializeField] private int lives = 5;
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int jumps = 2;
+    private int jumpsBuffer = 2;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+
+    }
+    private void FixedUpdate() 
+    {
+        if(CheckGround())
+            jumpsBuffer = jumps;
     }
     private void Update()
     {
         if(Input.GetButton("Horizontal"))
             Run();
-        if(Input.GetButtonDown("Jump"))
+        if((jumpsBuffer > 0) && Input.GetButtonDown("Jump"))
+        {
+            jumpsBuffer = jumpsBuffer - 1;
             Jump();
+        }
     }
     private void Run()
     {
@@ -31,5 +42,11 @@ public class hero : MonoBehaviour
     private void Jump()
     {
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+    
+    private bool CheckGround() 
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
+        return collider.Length > 1;
     }
 }
